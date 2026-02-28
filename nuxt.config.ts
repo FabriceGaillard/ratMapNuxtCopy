d0e684df560534bff609cfbfb84f887843a9cfba
Fabrice GAILLARD <fabou291@gmail.com>
2026-02-28 15:42:52 +0100

    feat: add @nuxtjs/color-mode module and update configurations

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,36 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/ui'
-  ],
+  modules: ["@nuxt/eslint", "@nuxt/ui", "@nuxtjs/color-mode"],
 
   devtools: {
-    enabled: true
+    enabled: true,
   },
 
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
 
   routeRules: {
-    '/': { prerender: false }
+    "/": { prerender: false },
   },
 
-  compatibilityDate: '2025-01-15',
+  compatibilityDate: "2025-01-15",
 
   eslint: {
     config: {
       stylistic: {
-        commaDangle: 'never',
-        braceStyle: '1tbs'
-      }
-    }
+        commaDangle: "never",
+        braceStyle: "1tbs",
+      },
+    },
   },
 
   runtimeConfig: {
     public: {
-      googleMapsKey: 'LEAKED_KEY_REMOVED'
-    }
+      googleMapsKey: "LEAKED_KEY_REMOVED",
+    },
   },
 
-  ssr: false
-})
+  ssr: false,
+
+  colorMode: {
+    preference: "light",
+    fallback: "light",
+    classSuffix: "",
+  },
+});
